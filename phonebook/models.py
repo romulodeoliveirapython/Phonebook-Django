cc8b32d13ac7ccfb5a0dbd41bd06d6a668414cd3
Romulo de Oliveira <romulodeoliveira1805@gmail.com>
2023-01-06 11:01:24 -0300

    Primeira configuração do model

--- phonebook/models.py
+++ phonebook/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Contato(models.Model):
+    nome = models.CharField(max_length = 30)
+    sobrenome = models.CharField(max_length = 30, blank = True)
+    empresa = models.CharField(max_length = 60, blank = True)
+    cargo = models.CharField(max_length = 60, blank = True)
+    email = models.CharField(max_length = 60, blank = True)
+    telefone = models.CharField(max_length = 15)
+    aniversário = models.DateField(blank = True)
+    observação = models.TextField(blank = True)
