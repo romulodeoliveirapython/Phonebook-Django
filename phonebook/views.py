f79e7005a8d3895fcdfc1faa88b0434fe8de3e43
Romulo de Oliveira <romulodeoliveira1805@gmail.com>
2023-01-08 19:29:29 -0300

    criação do list view

--- phonebook/views.py
+++ phonebook/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from phonebook.models import Contato
 
-# Create your views here.
+
+class ContatoList(ListView):
+    model = Contato
+    queryset = Contato.objects.all()
