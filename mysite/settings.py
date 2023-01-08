de1d21d287d9eceac5c3b989de825797a4da2846
Romulo de Oliveira <romulodeoliveira1805@gmail.com>
2023-01-08 19:27:37 -0300

    edição para o template

--- mysite/settings.py
+++ mysite/settings.py
@@ -1,4 +1,5 @@
 from .testing import secretkey, database
+import os
 
 """
 Django settings for mysite project.
@@ -57,7 +58,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
