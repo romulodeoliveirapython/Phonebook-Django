dab0be441cec9694a0c2a452ba629ca1205e6f63
Romulo de Oliveira <romulodeoliveira1805@gmail.com>
2023-01-08 21:02:41 -0300

    criação do create view

--- phonebook/views.py
+++ phonebook/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
 from phonebook.models import Contato
+from django.urls import reverse_lazy
 
 
 class ContatoList(ListView):
     model = Contato
     queryset = Contato.objects.all()
+
+class ContatoCreate(CreateView):
+    model = Contato
+    fields = '__all__'
+    success_url = reverse_lazy('phonebook:list')
