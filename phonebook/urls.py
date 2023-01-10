0c82ade17ae3e68a81158e71fa8e0a3eb2f824c5
Romulo de Oliveira <romulodeoliveira1805@gmail.com>
2023-01-10 11:47:54 -0300

    inicio da criação da view e rota para update dos contatos

--- phonebook/urls.py
+++ phonebook/urls.py
@@ -7,4 +7,5 @@ app_name = 'phonebook'
 urlpatterns = [
     path('list/', views.ContatoList.as_view(), name = 'list'),
     path('create/', views.ContatoCreate.as_view(), name = 'create'),
+    path('update/<int:pk>/', views.ContatoUpdate.as_view(), name = 'update'),
 ]
--- phonebook/views.py
+++ phonebook/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, UpdateView
 from phonebook.models import Contato
 from django.urls import reverse_lazy
 
@@ -8,7 +8,14 @@ class ContatoList(ListView):
     model = Contato
     queryset = Contato.objects.all()
 
+
 class ContatoCreate(CreateView):
     model = Contato
+    fields = '__all__'
+    success_url = reverse_lazy('phonebook:list')
+
+
+class ContatoUpdate(UpdateView):
+    model: Contato
     fields = '__all__'
     success_url = reverse_lazy('phonebook:list')
