5939ff51c10843164f7da5abf2a5e07b6a1d70c4
Romulo de Oliveira <romulodeoliveira1805@gmail.com>
2023-01-14 19:46:04 -0300

    alteração do settings para criação do sistema de autenticação do site

--- mysite/settings.py
+++ mysite/settings.py
@@ -120,3 +120,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGOUT_REDIRECT_URL = 'login'
+LOGIN_REDIRECT_URL = 'phonebook:list'
