002f56b0d080898cdd2fbd3ba204aaad4a86f34c
Romulo de Oliveira <romulodeoliveira1805@gmail.com>
2023-01-14 19:54:20 -0300

    criação da view para a home do site, erro 404 e criação dos decorators para a proteção de algumas views

--- phonebook/views.py
+++ phonebook/views.py
@@ -2,25 +2,36 @@ from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from phonebook.models import Contato
 from django.urls import reverse_lazy
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from urllib import request
 
 
+def home(request):
+    return render(request, 'phonebook/home.html')
+
+
+@method_decorator(login_required, name='dispatch')
 class ContatoList(ListView):
     model = Contato
     queryset = Contato.objects.all()
 
 
+@method_decorator(login_required, name='dispatch')
 class ContatoDetail(DetailView):
     model = Contato
     queryset = Contato.objects.all()
     template_name = 'phonebook/contato_detail.html'
 
 
+@method_decorator(login_required, name='dispatch')
 class ContatoCreate(CreateView):
     model = Contato
     fields = '__all__'
     success_url = reverse_lazy('phonebook:list')
 
 
+@method_decorator(login_required, name='dispatch')
 class ContatoUpdate(UpdateView):
     model: Contato
     fields = '__all__'
@@ -28,8 +39,13 @@ class ContatoUpdate(UpdateView):
     success_url = reverse_lazy('phonebook:list')
     template_name = 'phonebook/contato_update.html'
 
+
+@method_decorator(login_required, name='dispatch')
 class ContatoDelete(DeleteView):
     model = Contato
     fields = '__all__'
     success_url = reverse_lazy('phonebook:list')
-    
+
+
+def handler404(request, exception):
+    return render(request, 'phonebook/404.html')
