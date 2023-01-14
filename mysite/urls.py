42b9d3df724fc3c0db22c6d5d7f2374bfad73855
Romulo de Oliveira <romulodeoliveira1805@gmail.com>
2023-01-14 19:47:01 -0300

    configuração para o template do erro 404

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('contatos/', include('phonebook.urls')),
+    path('accounts/', include('django.contrib.auth.urls'))
 ]
+
+
+handler404 = "phonebook.views.handler404"
