c696a834079a155c1e474a695acaf1555d6c9c6e
Romulo de Oliveira <romulodeoliveira1805@gmail.com>
2023-01-19 23:27:17 -0300

    criação da logica para renderizar erros no template

--- phonebook/forms.py
+++ phonebook/forms.py
@@ -6,3 +6,9 @@ class ContatoForm(forms.ModelForm):
     class Meta:
         model = Contato
         exclude = ('user',)
+
+    def clean_telefone(self):
+        telefone = self.cleaned_data['telefone']
+        if not telefone.isdigit():
+            self.add_error('telefone', 'O número de telefone deve conter apenas caracteres numéricos!')
+        return telefone
